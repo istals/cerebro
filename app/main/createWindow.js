e53a3f06ded8123793f4d5d25ff00eea11054b5c
istals <ingus00stals@gmail.com>
2020-05-08 13:35:22 +0300

    upgrade electron and do releated changes

--- app/main/createWindow.js
+++ app/main/createWindow.js
@@ -142,15 +142,31 @@ export default ({ src, isDev }) => {
   app.on('activate', showMainWindow)
 
   // Someone tried to run a second instance, we should focus our window.
-  const shouldQuit = app.makeSingleInstance(() => {
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore()
-      mainWindow.focus()
-    }
-  })
+  if (app.makeSingleInstance) {
+    const shouldQuit = app.makeSingleInstance(() => {
+      if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore()
+        mainWindow.focus()
+      }
+    })
 
-  if (shouldQuit) {
-    app.quit()
+    if (shouldQuit) {
+      app.quit()
+    }
+  } else if (app.requestSingleInstanceLock) {
+    const gotTheLock = app.requestSingleInstanceLock()
+
+    if (!gotTheLock) {
+      app.quit()
+    } else {
+      app.on('second-instance', (event, commandLine, workingDirectory) => {
+        // Someone tried to run a second instance, we should focus our window.
+        if (mainWindow) {
+          if (mainWindow.isMinimized()) mainWindow.restore()
+          mainWindow.focus()
+        }
+      })
+    }
   }
 
   if (donateDialog.shouldShow()) {
